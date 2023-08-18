dc54390829bf2ef049ee54212d06fff18938a71c
MehdiH7 <mehdi.houshmand@hotmail.com>
2023-08-18 18:56:36 +0200

    YOLO Training code added

--- Train/train.py
+++ Train/train.py
@@ -1,5 +1,5 @@
 #Multi-GPU Training on YOLO
-
+import torch
 import time
 import logging
 import psutil
@@ -7,14 +7,22 @@ import GPUtil
 from tqdm import tqdm
 from ultralytics import YOLO
 import comet_ml
-import torch
 import utils
+
 
 # Initialize Logging
 logging.basicConfig(filename='training_log.log', level=logging.INFO)
 
 comet_ml.init(project_name='YOLOV8_Soccer')
-display = utils.notebook_init()
+
+if torch.cuda.is_available():
+    num_gpus = torch.cuda.device_count()
+    print(f"CUDA is available with {num_gpus} GPU{'s' if num_gpus > 1 else ''}.")
+
+    for i in range(torch.cuda.device_count()):
+        GPU_name = print(f"Device {i}: {torch.cuda.get_device_name(i)}")
+else:
+    print("CUDA is not available.")
 
 # Combine model paths and load flags into a single dictionary
 models_to_load = {
@@ -36,39 +44,38 @@ if selected_model_path:
 
     start_time = time.time()
 
-    for epoch in tqdm(range(100), desc="Training progress"):  # using tqdm for progress bar
-        # Check CPU and GPU usage
-        cpu_usage = psutil.cpu_percent()
-        gpu_usage = GPUtil.getGPUs()[0].load
-        logging.info(f"Epoch {epoch + 1}: CPU Usage: {cpu_usage}%, GPU Usage: {gpu_usage}%")
+    # Check CPU and GPU usage
+    cpu_usage = psutil.cpu_percent()
+    gpu_usage = GPUtil.getGPUs()[0].load
+    logging.info(f"Trinining will be started with {GPU_name} ")
 
-        # Train the model with 2 GPUs
-        results = model.train(
-            data = 'config.yaml',
-            project = 'YOLOV8_Soccer', #Project name
-            device = [0, 1],        #device to run on, i.e. cuda device=0 or device=0,1,2,3 or device=cpu
-            bbox_interval = 1       #log model predictions and the associated images by passing the bbox_interval command line argument. Predictions can be visualized using Comet's Object Detection Custom Panel
-            
-            #Model Hyperparameters
-            epochs = 100, 
-            imgsz = 640, 
-            device = [0, 1],        #device to run on, i.e. cuda device=0 or device=0,1,2,3 or device=cpu
-            patience = 50,
-            batch = 16,
-            save = True,
-            save_period = 5,
-            workers = 8,
-            pretrained = True,
-            optimizer = 'AdamW',    #choices=[SGD, Adam, Adamax, AdamW, NAdam, RAdam, RMSProp, auto]
-            verbose = True,
-            seed = 7,
-            cos_lr = True,          #use cosine learning rate scheduler
-            resume = True,
-            freeze = 1,             #(int or list, optional) freeze first n layers, or freeze list of layer indices during training
-            lr0 = 0.01,             #initial learning rate (i.e. SGD=1E-2, Adam=1E-3)
-            warmup_epochs = 3.0,
-            dropout = 0.5
-            )
+    # Train the model with 2 GPUs
+    results = model.train(
+        data = 'config.yaml',
+        project = 'YOLOV8_Soccer', #Project name
+        device = [0],        #device to run on, i.e. cuda device=0 or device=0,1,2,3 or device=cpu
+
+        #Model Hyperparameters
+        epochs = 10, 
+        imgsz = 640, 
+        patience = 50,
+        batch = 16,
+        save = True,
+        save_period = 5,
+        workers = 8,
+        optimizer = 'AdamW',    #choices=[SGD, Adam, Adamax, AdamW, NAdam, RAdam, RMSProp, auto]
+        verbose = True,
+        seed = 7,
+        cos_lr = True,          #use cosine learning rate scheduler
+        lr0 = 0.01,             #initial learning rate (i.e. SGD=1E-2, Adam=1E-3)
+        warmup_epochs = 3.0,
+        dropout = 0.5
+
+
+        #pretrained = True,
+        #freeze = 1,             #(int or list, optional) freeze first n layers, or freeze list of layer indices during training
+        #resume = True
+        )
     
     end_time = time.time()
     execution_time_minutes = (end_time - start_time) / 60
